aacdb2eef92300ab05d9ca65547ecd8e9db40a11
palucena <palucena@student.42malaga.com>
2023-04-23 19:15:06 +0200

    first commit

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,17 +6,36 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:50:34 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/20 18:01:17 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/23 19:03:38 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <libft.h>
+#include "libft.h"
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
+	int		j;
+	int		h;
 	char	*res;
 
 	i = 0;
+	while (ft_strchr(set, s1[i]) != 0)
+		i++;
+	j = ft_strlen(s1);
+	while (ft_strrchr(set, s1[j]) != 0)
+		j--;
+	res = malloc(sizeof(char) * (j - i + 2));
+	if (!res || !s1)
+		return (0);
+	h = 0;
+	while (i <= j)
+	{
+		res[h] = s1[i];
+		i++;
+		h++;
+	}
+	res[h] = 0;
+	return (res);
 }
