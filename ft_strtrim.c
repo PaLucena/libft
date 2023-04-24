d5667ea775b81f16b882503f7da3d9db449061c7
palucena <palucena@student.42malaga.com>
2023-04-24 15:45:10 +0200

    Francinette tested

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,36 +6,84 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 17:50:34 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/23 19:03:38 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/24 14:07:43 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include "libft.h"
 
+int	ft_start(char const *s1, char const *set)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		while (set[j])
+		{
+			if (s1[i] == set[j])
+			{
+				i++;
+				break ;
+			}
+			j++;
+		}
+		if (j == ft_strlen(set))
+			return (i);
+			j = 0;
+	}
+	return (0);
+}
+
+int	ft_finish(char const *s1, char const *set)
+{
+	int	len;
+	int	i;
+
+	len = 0;
+	i = 0;
+	while (s1[len])
+		len++;
+	while (len > 0)
+	{
+		while (set[i])
+		{
+			if (s1[len - 1] == set[i])
+			{
+				len--;
+				break ;
+			}
+			i++;
+		}
+		if (i == ft_strlen(set))
+			return (len);
+		i = 0;
+	}
+	return (0);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		i;
-	int		j;
-	int		h;
+	int		s;
+	int		f;
 	char	*res;
 
 	i = 0;
-	while (ft_strchr(set, s1[i]) != 0)
-		i++;
-	j = ft_strlen(s1);
-	while (ft_strrchr(set, s1[j]) != 0)
-		j--;
-	res = malloc(sizeof(char) * (j - i + 2));
-	if (!res || !s1)
+	s = ft_start(s1, set);
+	f = ft_finish(s1, set);
+	res = malloc(sizeof(char) * (f - s + 1));
+	if (!res)
 		return (0);
-	h = 0;
-	while (i <= j)
+	while (s < f)
 	{
-		res[h] = s1[i];
+		res[i] = s1[s];
 		i++;
-		h++;
+		s++;
 	}
-	res[h] = 0;
+	res[i] = 0;
 	return (res);
 }
