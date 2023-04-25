b72dcd35ec4d37a59edd7904b9bce08ccba025b5
palucena <palucena@student.42malaga.com>
2023-04-25 15:42:56 +0200

    Francinette tested

--- ft_split.c
+++ ft_split.c
@@ -6,11 +6,75 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 16:04:12 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/24 16:35:54 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/25 15:39:48 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
+int	ft_wordcount(char const *s, char c)
+{
+	int	count;
+
+	count = 0;
+	while (*s)
+	{
+		if (*s != c)
+		{
+			count++;
+			while (*s && *s != c)
+				s++;
+		}
+		else
+			s++;
+	}
+	return (count);
+}
+
+void	ft_free(char **str, int str_ind)
+{
+	while (str_ind-- > 0)
+		free(str[str_ind]);
+	free(str);
+}
+
+int	ft_len(char const *s, char c, int i)
+{
+	int	len;
+
+	len = 0;
+	while (s[i] && s[i] != c)
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+
 char	**ft_split(char const *s, char c)
 {
-	
+	char	**str;
+	int		i;
+	int		str_ind;
+
+	i = 0;
+	str_ind = -1;
+	str = malloc(sizeof(char *) * (ft_wordcount(s, c) + 1));
+	if (!str)
+		return (0);
+	while (++str_ind < ft_wordcount(s, c))
+	{
+		while (s[i] == c)
+			i++;
+		str[str_ind] = ft_substr(s, i, ft_len(s, c, i));
+		if (!(str[str_ind]))
+		{
+			ft_free(str, str_ind);
+			return (0);
+		}
+		i += ft_len(s, c, i);
+	}
+	str[str_ind] = 0;
+	return (str);
 }
