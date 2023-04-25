1cd5e43eb26c23300db4a83cbe96be8a13c84b64
palucena <palucena@student.42malaga.com>
2023-04-25 16:37:13 +0200

    Francinette tested

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,8 +6,48 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 15:43:34 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/25 15:43:54 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/25 16:35:33 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
+int	ft_count(long int n)
+{
+	int	len;
+
+	len = 1;
+	while (n >= 10)
+	{
+		len++;
+		n /= 10;
+	}
+	return (len);
+}
+
 char	*ft_itoa(int n)
+{
+	int			len;
+	long int	n2;
+	char		*n_char;
+
+	n2 = n;
+	if (n2 < 0)
+		n2 *= -1;
+	len = ft_count(n2);
+	if (n < 0)
+		len++;
+	n_char = malloc(sizeof(char) * (len + 1));
+	if (!n_char)
+		return (0);
+	n_char[len--] = 0;
+	while (len >= 0)
+	{
+		n_char[len--] = (n2 % 10) + '0';
+		n2 /= 10;
+	}
+	if (n < 0)
+		n_char[0] = '-';
+	return (n_char);
+}
