dba022019248d05421ca8b6253bb64f77ba6cfbd
palucena <palucena@student.42malaga.com>
2023-04-26 12:57:50 +0200

    Optimised

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 12:20:15 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/21 18:23:03 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/26 12:52:37 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,24 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*sub;
-	size_t	i;
 	size_t	j;
 
 	if ((size_t)ft_strlen(s) < start)
 		return (ft_strdup(""));
 	if ((size_t)ft_strlen(s) < start + len)
 		len = ft_strlen(s) - start;
-	sub = (char *)malloc((len + 1) * sizeof(char));
+	sub = malloc((len + 1) * sizeof(char));
 	if (!sub || !s)
 		return (0);
-	i = 0;
 	j = 0;
-	while (s[i])
+	while (s[start])
 	{
-		if (i >= start && j < len)
+		if (j < len)
 		{
-			sub[j] = s[i];
+			sub[j] = s[start];
 			j++;
 		}
-		i++;
+		start++;
 	}
 	sub[j] = 0;
 	return (sub);
