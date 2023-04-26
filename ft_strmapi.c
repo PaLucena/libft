141b9e7b46d846d8609d88db55021c3022e7cd49
palucena <palucena@student.42malaga.com>
2023-04-26 11:54:51 +0200

    Francinette tested

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,30 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:38:48 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/26 10:16:28 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:51:04 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-    
+	int		i;
+	char	*str;
+
+	if (!s || !f)
+		return (0);
+	i = ft_strlen(s);
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (0);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = 0;
+	return (str);
 }
