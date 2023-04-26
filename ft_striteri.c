9351a07e17bcdb61387ffd94bdd346ed93718c41
palucena <palucena@student.42malaga.com>
2023-04-26 12:06:46 +0200

    Francinette tested

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,10 +6,22 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/26 11:52:27 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/26 11:53:58 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/26 12:05:41 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	i;
+
+	if (!s)
+		return ;
+	i = 0;
+	while (s[i])
+	{
+		f(i, s + i);
+		i++;
+	}
+}
