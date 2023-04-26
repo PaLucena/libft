61c153a21580183400740c1bf7fbc4d9a04a8f63
palucena <palucena@student.42malaga.com>
2023-04-26 10:16:54 +0200

    Change

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,11 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/25 16:38:48 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/25 16:41:44 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/26 10:16:28 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+    
 }
