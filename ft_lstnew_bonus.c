4292f2cb2d169922a16379297bf47e1c0b03da4e
palucena <palucena@student.42malaga.com>
2023-04-27 12:46:10 +0200

    Francinette tested

---
+++ ft_lstnew_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/26 14:01:20 by palucena          #+#    #+#             */
+/*   Updated: 2023/04/27 12:30:47 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_l;
+
+	new_l = malloc(sizeof(t_list));
+	if (!new_l)
+		return (0);
+	new_l->content = content;
+	new_l->next = 0;
+	return (new_l);
+}
