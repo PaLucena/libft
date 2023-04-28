d268e63c92ad0d1ffb3c9bbe1956844cf07d3a2c
palucena <palucena@student.42malaga.com>
2023-04-28 15:43:15 +0200

    Finished

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,10 +6,11 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 13:55:40 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/28 14:56:33 by palucena         ###   ########.fr       */
+/*   Updated: 2023/04/28 15:12:37 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "libft.h"
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
