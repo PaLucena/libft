3df1a0da6d95f6c4d1551358990f74cbbb683c5f
palucena <palucena@student.42malaga.com>
2023-04-28 12:53:37 +0200

    First commit && francinetted

---
+++ ft_lstlast_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 12:20:51 by palucena          #+#    #+#             */
+/*   Updated: 2023/04/28 12:52:34 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*actual;
+
+	if (!lst)
+		return (0);
+	actual = lst;
+	while (actual->next)
+	{
+		actual = actual->next;
+	}
+	return (actual);
+}
