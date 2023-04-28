c8b157162c4bab6550fe36c0c7a62ea68dd8d79c
palucena <palucena@student.42malaga.com>
2023-04-28 13:54:35 +0200

    first commit && francinetted

---
+++ ft_lstiter_bonus.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstiter_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 13:47:35 by palucena          #+#    #+#             */
+/*   Updated: 2023/04/28 13:53:34 by palucena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
