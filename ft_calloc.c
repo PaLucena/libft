abbda537ee3d8b844238e32feaf92748f2ec680d
palucena <palucena@student.42malaga.com>
2023-05-08 18:08:22 +0200

    <3

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,11 +6,9 @@
 /*   By: palucena <palucena@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 12:18:43 by palucena          #+#    #+#             */
-/*   Updated: 2023/04/21 16:34:00 by palucena         ###   ########.fr       */
+/*   Updated: 2023/05/08 18:07:44 by palucena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdlib.h>
 
 void	*ft_calloc(size_t nb, size_t size)
 {
